a81292728eecff4f5cd71373c8e5ad2674725b67
AndrmattR24 <andrewmatiasrengifo@gmail.com>
2024-06-25 20:38:25 -0500

    creacion, configuracion y estructuracion de carpetas

---
+++ src/main/java/group5/idat/project_sgei/ProjectSgeiApplication.java
@@ -0,0 +1,13 @@
+package group5.idat.project_sgei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSgeiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSgeiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/group5/idat/project_sgei/ProjectSgeiApplicationTests.java
@@ -0,0 +1,13 @@
+package group5.idat.project_sgei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSgeiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
